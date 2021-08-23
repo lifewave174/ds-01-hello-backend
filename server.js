f4744aad4d41533b21c596b148e5f2014655f2b5
dci <priya.shah174@gmail.com>
2021-08-23 14:32:23 +0200

    sesent html list format to the browser using response.json and added a header called backend

--- server.js
+++ server.js
@@ -23,3 +23,9 @@ app.get("/text", (request, response) => {
     const stringFormat = dciCourseModules.join(" ")
     response.json(stringFormat)
 })
+
+app.get("/html", (request, response) => {
+    let listFormat = dciCourseModules.map(module => `<li>${module}</li>`).join(" ")
+    listFormat = `<b>Backend</b> <ul>${listFormat}</ul>`
+    response.send(listFormat)
+})
