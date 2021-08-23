c718550f6eeb0a06e0c9b599b0db1f4b835c0039
dci <priya.shah174@gmail.com>
2021-08-23 14:16:49 +0200

    sent a string response to the browser

--- server.js
+++ server.js
@@ -18,3 +18,8 @@ app.listen(5000, () => {
 app.get("/json", (request, response) => {
     response.json(dciCourseModules)
 })
+
+app.get("/text", (request, response) => {
+    const stringFormat = dciCourseModules.join(" ")
+    response.json(stringFormat)
+})
