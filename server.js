e930ae50b7ad0c62f9cf2312c3a11fab134c6049
dci <priya.shah174@gmail.com>
2021-08-23 14:11:24 +0200

    ExJSON response sent to the browser

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 const express = require("express")
 const app = express()
 
+dciCourseModules = [
+    "HTML/CSS",
+    "Javascript",
+    "DOM",
+    "SPA",
+    "Backend",
+    "Fullstack",
+    "Final Project"
+]
+
 app.listen(5000, () => {
     console.log("The server has now started!")
+})
+
+app.get("/json", (request, response) => {
+    response.json(dciCourseModules)
 })
